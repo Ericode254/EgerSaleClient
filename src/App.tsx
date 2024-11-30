8bce2a19d1b47bc71e86561e4a59cf2659850a00
Ericode254 <jilloerick6@gmail.com>
2024-11-30 05:03:33 +0300

    protect the home route from unathorized users

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,25 @@ import Home from './pages/main/Home'
 import SignUp from './pages/auth/SignUp'
 import SignIn from './pages/auth/SignIn'
 import About from './pages/about/About'
+import ForgotPassword from './pages/auth/ForgotPassword'
+import ResetPassword from './pages/auth/ResetPassword'
+import { AuthProvider } from './pages/auth/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/auth/signup" element={<SignUp />} />
-        <Route path="/auth/signin" element={<SignIn />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/auth/signup" element={<SignUp />} />
+          <Route path="/auth/signin" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path='/auth/forgot-password' element={<ForgotPassword />} />
+          <Route path='/auth/reset-password' element={<ResetPassword />} />
+        </Routes>
+      </AuthProvider>
     </>
   )
 }
